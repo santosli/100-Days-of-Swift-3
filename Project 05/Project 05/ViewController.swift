785bdf991d0faaaee8661f03490e8d0d812ecd90
lituo.lt <lituo.lt@alibaba-inc.com>
2016-10-28 17:34:09 +0800

    05

---
+++ Project 05/Project 05/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Project 05
+//
+//  Created by Santos on 28/10/2016.
+//  Copyright © 2016 santos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
